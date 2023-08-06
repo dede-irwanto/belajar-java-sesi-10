ca43296cb0c4d0f87da1493e2b84c584d6b4eb29
dede-irwanto <id.dedeirwanto@gmail.com>
2023-08-06 20:34:34 +0800

    Belajar Java Unit Test

--- src/test/java/programmer/zaman/now/test/CalculatorTest.java
+++ src/test/java/programmer/zaman/now/test/CalculatorTest.java
@@ -2,11 +2,28 @@ package programmer.zaman.now.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.opentest4j.TestAbortedException;
+
 @DisplayName("Test untuk Calculator class")
 public class CalculatorTest {
     private final Calculator calculator = new Calculator();
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all");
+    }
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After all");
+    }
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before each");
+    }
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After each");
+    }
     @Test
     @DisplayName("Test skenario sukses untuk method add(Integer, Integer)")
     public void testAddSuccess() {
@@ -23,5 +40,18 @@ public class CalculatorTest {
         assertThrows(IllegalArgumentException.class, () -> {
             calculator.devide(10,0);
         });
+    }
+    @Test
+    @Disabled
+    public void testComingSoon() {
+    }
+    @Test
+    public void testAborted() {
+        var profile = System.getenv("PROFILE");
+        if (!"DEV".equals(profile)) {
+            throw new TestAbortedException("Test dibatalkan karena bukan DEV");
+        }
+
+        // Unit test untuk DEV
     }
 }
